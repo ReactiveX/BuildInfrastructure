7ba1eb752540ffe12ecc4f21c11874469fe0899a
Ben Christensen <benjchristensen@gmail.com>
2014-09-01 14:59:52 -0700

    Update publishViaTravis.sh

--- gradle/publishViaTravis.sh
+++ gradle/publishViaTravis.sh
@@ -6,7 +6,7 @@ echo "PULL REQUEST $TRAVIS_PULL_REQUEST and Branch $TRAVIS_BRANCH"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Starting publish to Sonatype...\n"
 
-  ./gradlew candidate -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}"
+  ./gradlew candidate -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" --stacktrace
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then
