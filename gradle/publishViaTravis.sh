90f833794e33a6ada22776422a3ea3edeb1fae3d
Ben Christensen <benjchristensen@gmail.com>
2014-09-01 19:32:23 -0700

    Update publishViaTravis.sh

--- gradle/publishViaTravis.sh
+++ gradle/publishViaTravis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This script will upload to Bintray. It is intended to be conditionally executed on tagged builds.
 
-
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e "Starting upload to Bintray on Branch "$TRAVIS_BRANCH"" and Tag "$TRAVIS_TAG" ...\n"
   
