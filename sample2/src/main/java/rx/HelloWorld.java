cfaa6329f0b21624e963c8fcd56f8367306bdbd3
Justin Ryan <jryan@netflix.com>
2014-11-24 13:11:17 -0800

    Construct a multi-module project to test with.

---
+++ sample2/src/main/java/rx/HelloWorld.java
@@ -0,0 +1,11 @@
+package rx;
+
+public class HelloWorld {
+
+    public static final String msg = "hello world";
+
+    public static void main(String[] args) {
+        System.out.println(msg);
+    }
+}
+
