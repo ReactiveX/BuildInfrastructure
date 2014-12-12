b8236c9183169fc7c726596a06d5fef9a187be6e
Justin Ryan <jryan@netflix.com>
2014-12-12 14:37:44 -0800

    Reference 2.2

--- bump.sh
+++ bump.sh
@@ -1,3 +1,4 @@
-perl -p -i -e 's/(\d+)/1 + \1 /ge' gradle.properties
+#perl -p -i -e 's/(\d+)/1 + \1 /ge' gradle.properties
+perl -p -i -e 's/(counter=)(\d+)/$1.($2+1)/ge' gradle.properties
 git add gradle.properties
 git commit -m "Bumping counter"
