0048f990680ee32976a7ef4431936e7df40aa742
Rob Spieldenner <rob.spieldenner@gmail.com>
2015-01-19 13:53:05 -0800

    Tweak build scripts

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -4,11 +4,3 @@
 echo -e 'Build Script => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
 
 ./gradlew -Prelease.useLastTag=true build
-RETVAL=$?
-
-if [ $RETVAL -eq 0 ]; then
-  echo 'Completed build!'
-else
-  echo 'Build failed.'
-  false
-fi
--- gradle/publishViaTravis.sh
+++ gradle/publishViaTravis.sh
@@ -7,14 +7,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Bintray Upload => Starting upload ...\n'
   
   ./gradlew -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" final --stacktrace
-  RETVAL=$?
-  
-  if [ $RETVAL -eq 0 ]; then
-    echo 'Completed upload!'
-  else
-    echo 'Upload failed.'
-    false
-  fi
 else
   echo 'Bintray Upload => Not a tagged build so will not upload' 
 fi
